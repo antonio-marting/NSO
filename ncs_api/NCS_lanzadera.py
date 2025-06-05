a79b37ab83a54c3bf4381bc3a0de4641efb537ea
antonio.marting@gmail.com <antonio.marting@gmail.com>
2025-06-05 17:14:08 +0200

    daily update

--- ncs_api/NCS_lanzadera.py
+++ ncs_api/NCS_lanzadera.py
@@ -2,19 +2,18 @@ import ncs, _ncs
 
 ''' This script lists the rollbacks available in the NCS system.'''
 
-with ncs.maapi.single_read_trans('admin', 'python') as t:
+with ncs.maapi.single_write_trans('admin', 'python') as t:
     rollbacks = _ncs.maapi.list_rollbacks(t.maapi.msock, 100)
-    
+
     for rollback in rollbacks:
         print(f'--------- ROLLBACK {rollback.nr} -------------')
         print(rollback)
-        
         print('Comment:', rollback.comment)
         print('Label:', rollback.label)
         print('user: ', rollback.creator)
         print('method: ', rollback.via)
         print('Fixed-number: ', rollback.fixed_nr)
         print('date: ', rollback.datestr)
-        
-            
-    
+
+
+    print(ncs.maapi.COMMIT_NCS_NO_NETWORKING)
