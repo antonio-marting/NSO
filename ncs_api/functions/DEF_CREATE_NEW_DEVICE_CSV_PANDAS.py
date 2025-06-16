0ec5bd034135d81179b98749c4b95b84e35294a4
antonio.marting@gmail.com <antonio.marting@gmail.com>
2025-06-16 18:16:09 +0200

    updating creating list devices w/Pandas/CSV

--- ncs_api/functions/DEF_CREATE_NEW_DEVICE_CSV_PANDAS.py
+++ ncs_api/functions/DEF_CREATE_NEW_DEVICE_CSV_PANDAS.py
@@ -58,4 +58,14 @@ def onboard_devices():
                     dev_sync = device[device_name].sync_from()
                     print(f'        Result: {dev_sync.result} {dev_sync.info}')
 
+                # Open Read Transaction to list all devices in NSO
+                with ncs.maapi.single_read_trans('admin', 'python') as t:
+                    root = ncs.maagic.get_root(t)
+                    devices = root.devices.device
+                    print('-' * 60)
+                    print('| {0:20} | {1:15} | {2:15} |'.format('DEVICE', 'IPv4 ADDRESS', 'AUTHGROUP'))
+                    print('-' * 60)
+                    for dev_data in devices:
+                        print('| {0:20} | {1:15} | {2:15} |'.format(dev_data.name, dev_data.address, dev_data.authgroup))
+
 onboard_devices()
