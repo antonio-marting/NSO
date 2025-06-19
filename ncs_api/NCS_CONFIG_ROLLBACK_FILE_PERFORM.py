ea5d453d49ee2301454c79907abef0ca883de5ab
antonio.marting@gmail.com <antonio.marting@gmail.com>
2025-06-19 16:53:02 +0200

    show rollback changes

---
+++ ncs_api/NCS_CONFIG_ROLLBACK_FILE_PERFORM.py
@@ -0,0 +1,17 @@
+import ncs
+
+''' This script perform Rollback to a change with a especific fixed-number'''
+
+with ncs.maapi.single_read_trans('admin', 'python') as t:
+    root = ncs.maagic.get_root(t)
+
+    rollback_apply_params = root.rollback__rollback_files.rollback__apply_rollback_file.get_input()
+    rollback_apply_params.fixed_number = 10168
+    rollback_apply_params.comment = f'Comment Maapi APPLY_ROLBACK_FILES {rollback_apply_params.fixed_number}'
+    rollback_apply_params.label = f'Label Maapi APPLY_ROLBACK_FILES {rollback_apply_params.fixed_number}'  
+    
+
+    apply_rollback = root.rollback__rollback_files.rollback__apply_rollback_file(rollback_apply_params)
+    print(f'Rollback change # {rollback_apply_params.fixed_number} .... Done...!!!')
+    print(apply_rollback.ncs_rollback__cli.local_node.data)
+    
